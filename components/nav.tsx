647fc00e068c4c9477c2a7890d8bdf17276f0ff8
Odun Freeman <bornduringwar@gmail.com>
2022-09-22 19:25:13 -0400

    userAccount Page redirects if no session,

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from 'next';
 import Link from 'next/link';
 import { Button } from '@mui/material';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Nav: NextPage = () => {
+  const router = useRouter();
+
   const { data: session } = useSession();
   return (
     <nav className="w-screen border border-black p-4 flex justify-center gap-4">
@@ -16,8 +20,17 @@ const Nav: NextPage = () => {
           Login
         </Button>
       ) : (
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-3 bg-white p-4">
           <p>Welcome, {session?.user?.name}</p>
+          <Image
+            src={session.user.image}
+            width={'50px'}
+            height={'50px'}
+            className="rounded-full"
+          />
+          <Link href={'/user/account'}>
+            <Button variant="outlined">Account Page</Button>
+          </Link>
           <Button variant="outlined" onClick={() => signOut()}>
             Log Out
           </Button>
---
+++ pages/user/account.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Nav from '../../components/nav';
+import { useSession, signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const SignUp: NextPage = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+  useEffect(() => {
+    if (!session) {
+      router.push('/');
+    }
+  });
+  if (session) {
+    return (
+      <div>
+        <Head>
+          <title>FPC Accoutn</title>
+          <meta name="description" content="Sign up for Fit People Cook" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="bg-blue-500 min-h-screen">
+          <Nav />
+          <div className="font-bold text-4xl text-center">
+            Welcome to your homepage {session.user.name}
+          </div>
+        </main>
+      </div>
+    );
+  } else {
+    return <div>You are not signed in</div>;
+  }
+};
+
+export default SignUp;
